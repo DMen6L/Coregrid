f1e27e49808d2e2dfe77dbad48b19eab091100b5
DMen6L <240103073@sdu.edu.kz>
2026-06-02 17:10:50 +0500

    feat: updating fastapi CRUD format
    using Depends to stop creating individul sessions each time, response
    models for the endl=points and new patch endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from os import name
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
 from sqlalchemy import text
+from sqlalchemy.orm import Session
 
 from app.db import SessionLocal, engine, Base
 from app.models import Company, Supplier, Product
@@ -13,6 +14,12 @@ from app.schemas import (
     ProductCreate,
     ProductResponse,
 )
+
+
+def get_db():
+    with SessionLocal() as session:
+        yield session
+
 
 app = FastAPI()
 
@@ -28,48 +35,55 @@ def read_root():
 # =========================
 
 
-@app.post("/add_company", status_code=201)
-def add_company(company_data: CompanyCreate):
-    with SessionLocal() as session:
-        new_company = Company(iin=company_data.iin, name=company_data.name)
+@app.post("/companies", response_model=CompanyResponse, status_code=201)
+def add_company(company_data: CompanyCreate, db: Session = Depends(get_db)):
+    new_company = Company(iin=company_data.iin, name=company_data.name)
 
-        session.add(new_company)
-        session.commit()
-        session.refresh(new_company)
+    db.add(new_company)
+    db.commit()
+    db.refresh(new_company)
 
-        return {"Success": new_company}
-
-
-@app.post("/add_supplier", status_code=201)
-def add_supplier(supplier_data: SupplierCreate):
-    with SessionLocal() as session:
-        new_supplier = Supplier(
-            name=supplier_data.name, phone_number=supplier_data.phone_number
-        )
-
-        session.add(new_supplier)
-        session.commit()
-        session.refresh(new_supplier)
-
-        return {"Success": new_supplier}
+    return new_company
 
 
-@app.post("/add_product", status_code=201)
-def add_product(product_data: ProductCreate):
-    with SessionLocal() as session:
-        new_product = Product(
-            name=product_data.name,
-            price=product_data.price,
-            quantity=product_data.quantity,
-            company_id=product_data.company_id,
-            supplier_id=product_data.supplier_id,
-        )
+@app.post("/suppliers", response_model=SupplierResponse, status_code=201)
+def add_supplier(supplier_data: SupplierCreate, db: Session = Depends(get_db)):
+    new_supplier = Supplier(
+        name=supplier_data.name, phone_number=supplier_data.phone_number
+    )
 
-        session.add(new_product)
-        session.commit()
-        session.refresh(new_product)
+    db.add(new_supplier)
+    db.commit()
+    db.refresh(new_supplier)
 
-        return {"Success": new_product}
+    return new_supplier
+
+
+@app.post("/products", response_model=ProductResponse, status_code=201)
+def add_product(product_data: ProductCreate, db: Session = Depends(get_db)):
+    new_product = Product(
+        name=product_data.name,
+        price=product_data.price,
+        quantity=product_data.quantity,
+        company_id=product_data.company_id,
+        supplier_id=product_data.supplier_id,
+    )
+
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+
+    return new_product
+
+
+# ===============
+# UPDATING TABLES
+# ===============
+
+
+@app.patch("/companies/{id}", status_code=200)
+def update_company(id: int):
+    pass
 
 
 # ===================
@@ -78,11 +92,8 @@ def add_product(product_data: ProductCreate):
 
 
 @app.delete("/cleanup", status_code=204)
-def cleanup():
-    with SessionLocal() as session:
-        _ = session.execute(
-            text(
-                "TRUNCATE TABLE products, companies, suppliers RESTART IDENTITY CASCADE"
-            )
-        )
-        session.commit()
+def cleanup(db: Session = Depends(get_db)):
+    db.execute(
+        text("TRUNCATE TABLE products, companies, suppliers RESTART IDENTITY CASCADE")
+    )
+    db.commit()
